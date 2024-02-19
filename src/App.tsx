e946b17b11d2ec983ae38f6fd43fbda3cccc53f3
Ida Casperson <ida_casperson@hotmail.com>
2024-02-19 23:16:51 +0100

    Fixed som style for input and text

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodoList from "./TodoList";
 function App() {
   return (
     <>
-      <header className="text-center md:text-6xl bg-slate-400 text-slate-50">
+      <header className="mb-4 text-4xl text-center md:text-6xl bg-slate-400 text-slate-50">
         <h1>Todo List</h1>
       </header>
       <main className="bg-slate-400">
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -42,50 +42,53 @@ const TodoList: React.FC = () => {
   };
 
   return (
-    <div className="text-center mx-auto max-w-md">
-      <h2 className="text-center mb-4 text-lg md:mt-8 underline">
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-center text-white mb-4 text-lg md:mt-8 underline">
         Add a todo here:
       </h2>
-      <input
-        type="text"
-        className=" border-gray-400 p-2 rounded "
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
+      <div className="flex">
+        <input
+          type="text"
+          className="border-gray-400 p-2 rounded"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button
+          onClick={addTask}
+          className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800 ml-3"
+        >
+          Add
+        </button>
+      </div>
 
-      <button
-        onClick={addTask}
-        className="bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-800 mb-4 ml-3"
-      >
-        Add
-      </button>
-
-      <ul>
+      <div className="w-full max-w-md mt-4">
         {tasks.map((task) => (
-          <li key={task.id}>
-            <input
-              type="checkbox"
-              className="mr-2 h-5 w-5"
-              checked={task.completed}
-              onChange={() => toggleTaskCompletion(task.id)}
-            />
-            <span
-              style={{
-                textDecoration: task.completed ? "line-through" : "none",
-              }}
-              className="text-lg"
-            >
-              {task.text}
-            </span>
+          <div
+            key={task.id}
+            className="flex items-center justify-between bg-gray-100 p-4 mb-4 rounded"
+          >
+            <div>
+              <input
+                type="checkbox"
+                className="mr-2 h-5 w-5"
+                checked={task.completed}
+                onChange={() => toggleTaskCompletion(task.id)}
+              />
+              <span
+                className={`text-lg ${task.completed ? "line-through" : ""}`}
+              >
+                {task.text}
+              </span>
+            </div>
             <button
               className="ml-3 px-4 py-2 rounded text-white bg-red-800"
               onClick={() => deleteTask(task.id)}
             >
               Delete
             </button>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
